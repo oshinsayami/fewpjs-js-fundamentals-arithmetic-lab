6b433707ea28f6dbc25e6111a1360ad648f0feb7
Simran <mdrsimran@gmail.com>
2021-02-07 22:57:53 -0600

    commit tree e9bf9467fc75ed5b1d0cf723c405b214b3a3797c

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+var num1 = 62;
+var num2 = 1;
+var num3 = 10;
+var num4 = 6;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random()) + 1;
+
+const mod = num3 % num4;
+
+const max = Math.max(1,2,3,20)
